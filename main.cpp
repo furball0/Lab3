2bc9393cf078d85aba030ba49f3190f5a8bc1a0a
Denis <denis-yurochkin@mail.ru>
2021-06-01 12:27:00 +0300

    init

--- main.cpp
+++ main.cpp
@@ -1,17 +1,4 @@
-#include "Speed_Test.h"
-//#include "MenuFull.h"
+#include "MenuFull.h"
 int main(){
-    test_find_bin_slow();
-//    menuFull();
-//    for(int i = 1000; i < 1001; i++){
-//        auto* tree = new Tree3<double>;
-//        int k = i / 2;
-//        for(int j = 0; j < k; j++){
-//            tree->Add(sin(j));
-//            tree->Add(-sin(j));
-//        }
-//        cout<<tree;
-//    }
-//    menuFull();
-
+    menuFull();
 }
